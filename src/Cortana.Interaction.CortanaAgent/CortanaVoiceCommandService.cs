32121fb5bc4327f089c2fe28e05e9601f554ca4f
Thomas Gobin <thomas.gobin@msp-france.com>
2016-03-03 17:57:00 +0100

    Terminer partie integration, début partie interaction

--- src/Cortana.Interaction.CortanaAgent/CortanaVoiceCommandService.cs
+++ src/Cortana.Interaction.CortanaAgent/CortanaVoiceCommandService.cs
@@ -7,7 +7,7 @@ using Windows.ApplicationModel.AppService;
 using Windows.ApplicationModel.Background;
 using Windows.ApplicationModel.VoiceCommands;
 
-namespace Cortana.Integrated.CortanaAgent
+namespace Cortana.Interaction.CortanaAgent
 {
     public sealed class CortanaVoiceCommandService : IBackgroundTask
     {
@@ -111,19 +111,105 @@ namespace Cortana.Integrated.CortanaAgent
 
             string serie = command.Properties["serie"][0];
 
-            var tempSerie = _series.FirstOrDefault(x => x.Value == serie);
+            var selectedSeries = _series.Where(x => x.Value.Contains(serie));
 
-            if (_series.ContainsValue(serie))
+            if (selectedSeries.Count() == 1)
             {
-                var userMessage = new VoiceCommandUserMessage();
-                userMessage.SpokenMessage = "Affichage de la série";
+                //Si il y a seulement une série correspondante, alors nous allons demander une confirmation (juste pour l'exemple)
+                var userPrompt = new VoiceCommandUserMessage();
+                userPrompt.SpokenMessage = userPrompt.DisplayMessage = "Est-ce bien la série que vous voulez afficher?";
 
-                var tempResponse = VoiceCommandResponse.CreateResponse(userMessage);
+                var userReprompt = new VoiceCommandUserMessage();
+                userReprompt.SpokenMessage = userReprompt.DisplayMessage = "Confirmez-vous que c'est bien la série que vous voulez afficher?";
 
-                tempResponse.AppLaunchArgument = "serie-" + _series.First(x => x.Value == serie).Key;
 
-                await voiceServiceConnection.RequestAppLaunchAsync(tempResponse);
+                //Création de la liste de résultats à afficher par Cortana
+                var destinationsContentTiles = new List<VoiceCommandContentTile>();
+
+                var tile = new VoiceCommandContentTile();
+                tile.ContentTileType = VoiceCommandContentTileType.TitleOnly;
+                tile.AppLaunchArgument = "serie-" + selectedSeries.First().Key;
+                tile.Title = selectedSeries.First().Value;
+
+                destinationsContentTiles.Add(tile);
+
+                var response = VoiceCommandResponse.CreateResponseForPrompt(userPrompt, userReprompt, destinationsContentTiles);
+
+                response.AppLaunchArgument = "serie-"+selectedSeries.First().Key;
+
+                var responseConfirmation = await voiceServiceConnection.RequestConfirmationAsync(response);
+
+                if(responseConfirmation != null && responseConfirmation.Confirmed)
+                {
+                    var userMessage = new VoiceCommandUserMessage();
+                    userMessage.SpokenMessage = "Lancement de l'application";
+
+                    var tempesponse = VoiceCommandResponse.CreateResponse(userMessage);
+
+                    response.AppLaunchArgument = "serie-" + selectedSeries.First().Key;
+
+                    await voiceServiceConnection.RequestAppLaunchAsync(response);
+                }
+                else
+                {
+                    //Si ce n'est pas cette série, alors on affiche les autres séries
+                    ShowSeries();
+                }
             }
+            else if (selectedSeries.Count() > 1)
+            {
+                //Levée d'ambigüité
+
+                //Création de la liste de résultats à afficher par Cortana
+                var destinationsContentTiles = new List<VoiceCommandContentTile>();
+
+                //Création d'une tuile pour chaque série
+                foreach (var tempSerie in selectedSeries)
+                {
+                    var tile = new VoiceCommandContentTile();
+                    tile.ContentTileType = VoiceCommandContentTileType.TitleOnly;
+                    tile.AppLaunchArgument = "serie-" + tempSerie.Key;
+                    tile.Title = tempSerie.Value;
+
+                    destinationsContentTiles.Add(tile);
+                }
+
+                var userPrompt = new VoiceCommandUserMessage();
+                userPrompt.DisplayMessage = "Afficher une série";
+                userPrompt.SpokenMessage = "Précisez la série que vous voulez afficher";
+
+                var userReprompt = new VoiceCommandUserMessage();
+                userReprompt.DisplayMessage = "Quelle série voulez-vous afficher?";
+                userReprompt.SpokenMessage = "Quelle série voulez-vous afficher?";
+
+                var disambiguationResponse = VoiceCommandResponse.CreateResponseForPrompt(userPrompt, userReprompt, destinationsContentTiles);
+
+                var result = await voiceServiceConnection.RequestDisambiguationAsync(disambiguationResponse);
+
+                if (result != null)
+                {
+                    var userMessage = new VoiceCommandUserMessage();
+                    userMessage.SpokenMessage = "Lancement de l'application";
+
+                    var response = VoiceCommandResponse.CreateResponse(userMessage);
+
+                    response.AppLaunchArgument = result.SelectedItem.AppLaunchArgument;
+
+                    await voiceServiceConnection.RequestAppLaunchAsync(response);
+                }
+            }
+            else
+            {
+                //Message d'erreur
+                var errorMessage = new VoiceCommandUserMessage();
+                errorMessage.DisplayMessage = "Aucune série ne correspond";
+                errorMessage.SpokenMessage = "Aucune série ne correspond";
+
+                var response = VoiceCommandResponse.CreateResponse(errorMessage);
+
+                await voiceServiceConnection.ReportFailureAsync(response);
+            }
+            
         }
 
         private async Task LaunchAppInForeground()
